94e07d7689c9c046c964d1bcf742a16fd1d2b114
Juno <ganimedes409@gmail.com>
2023-02-11 21:45:46 -0300

    reafctor: ensure SignUpController.handleSignUp implment correct interfaces

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,6 +1,7 @@
+import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignUpController {
-  handleSignUp (httpRequest: any): any {
+  handleSignUp (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
@@ -12,6 +13,10 @@ export class SignUpController {
         statusCode: 400,
         body: new Error('Email is required')
       }
+    }
+    return {
+      statusCode: 400,
+      body: new Error('Email is required')
     }
   }
 }
