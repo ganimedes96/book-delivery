397de7ed1aba3eb662ef5750da2683094ff22d26
Juno <ganimedes409@gmail.com>
2023-02-11 21:39:34 -0300

    test: ensure SignUpController return 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -14,4 +14,17 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new Error('Name is required'))
     expect(httpResponse.statusCode).toBe(400)
   })
+  it('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handleSignUp(httpRequest)
+    expect(httpResponse.body).toEqual(new Error('Email is required'))
+    expect(httpResponse.statusCode).toBe(400)
+  })
 })
