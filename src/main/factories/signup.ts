2fc3a168eec5a60e7f57b585cc887912edaa2085
Juno <ganimedes409@gmail.com>
2023-02-16 19:40:14 -0300

    feat: add SignUp Factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,15 @@
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
+import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
+import { AccountPrismaRepository } from '../../infra/db/prisma/account-repository/account'
+import { SignUpController } from '../../presentation/controllers/signup/signup.controller'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+
+export const makeSigUpController = (): SignUpController => {
+  const salt = 12
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const accountPrismaRepository = new AccountPrismaRepository()
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountPrismaRepository)
+  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+  return signUpController
+}
