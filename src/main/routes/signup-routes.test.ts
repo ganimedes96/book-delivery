e9e772e91f1b5141a04f6d1832a6b3e22ee4d4dc
Juno <ganimedes409@gmail.com>
2023-02-16 18:23:23 -0300

    refactor: conect to prisma before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,14 @@
+import { PrismaClient } from '@prisma/client'
 import request from 'supertest'
 import app from '../config/app'
 
 describe('SignUp Routes', () => {
+  const prisma = new PrismaClient()
+
+  beforeEach(async () => {
+    const costumers = prisma.costumer
+    await costumers.deleteMany({})
+  })
   it('should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
