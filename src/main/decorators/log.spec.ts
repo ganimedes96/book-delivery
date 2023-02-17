8dbb0f3528cf5144bdc22d3179a182df9b803d7f
Juno <ganimedes409@gmail.com>
2023-02-17 11:17:22 -0300

    rafactor: move sut creation toa factory method

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,28 +1,45 @@
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log'
 
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handleSignUp (HttpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: {
+          name: 'hudson'
+        }
+      }
+      return await new Promise(resolve => { resolve(httpResponse) })
+    }
+  }
+  return new ControllerStub()
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController()
+  const sut = new LogControllerDecorator(controllerStub)
+  return {
+    sut,
+    controllerStub
+  }
+}
+
 describe('LogController Decorator', () => {
   it('Should call controller handleSignUp', async () => {
-    class ControllerStub implements Controller {
-      async handleSignUp (HttpRequest: HttpRequest): Promise<HttpResponse> {
-        const httpResponse: HttpResponse = {
-          statusCode: 200,
-          body: {
-            name: 'hudson'
-          }
-        }
-        return await new Promise(resolve => { resolve(httpResponse) })
-      }
-    }
-    const controllerStub = new ControllerStub()
+    const { controllerStub, sut } = makeSut()
     const handleSignUpSpy = jest.spyOn(controllerStub, 'handleSignUp')
 
-    const sut = new LogControllerDecorator(controllerStub)
     const httpRequest = {
       body: {
         name: 'any_name',
         email: 'any_email@gmail.com',
-        passwword: 'eny_password',
+        password: 'eny_password',
         passwordConfirmation: 'any_passwordConfirmation',
         role: 'any_role'
       }
